72854f6d55bd55f7a71db8b881df629be8925138
Janice Chen <janicekchen@Janices-MacBook-Pro-2.local>
2020-05-02 13:16:34 -0400

    correct hyperlinks

--- critRS/02_scripts/02_forcenetwork.R
+++ critRS/02_scripts/02_forcenetwork.R
@@ -4,32 +4,40 @@ library(magrittr)
 library(igraph)
 
 
-# loading in edge data 
+# LOADING IN EDGE DATA
 bennett <- read.csv("03_data/02_citationedges.csv", stringsAsFactors = FALSE) %>% mutate(value = 2, Target = 1:n(), Source = 0)
 
-Encoding(bennett$citing) <- "UTF-8"
-Encoding(bennett$title) <- "UTF-8"
-
-test <- iconv(bennett$citing, from = "latin1", to = "UTF-8")
-test
-Encoding(test) <- "UTF-8"
-
-x <- c("Ekstr\xf8m", "J\xf6reskog", "bi\xdfchen Z\xfcrcher")
-Encoding(x) <- "ISO_8859-1"
-x
+# CREATING A NODES OBJECT
 bennett_nodes <- append(unique(bennett$cited_by), unique(bennett$citing)) %>%
   as.data.frame() 
-#%>%
- # mutate(nodeID = 0:(n()-1))
 
+# renaming author column 
 names(bennett_nodes)[1]<- "name"
+
+# joining node attributes to nodes object
 bennett_nodes %<>% left_join(bennett, by = c("name" = "citing"))
-bennett_nodes$citecount[1] <- 1
+
+# filling in Bennett data
+bennett_nodes$citecount[1] <- 1 
 bennett_nodes$category[1] <- "RSP"
 
+# adding category column with more meaningful names
+bennett_nodes$category_full <- ifelse(bennett_nodes$category == "RSP", "Remote Sensing Politics",
+                                      ifelse(bennett_nodes$category == "CGIS", "Critical GIS", 
+                                             ifelse(bennett_nodes$category == "RSA", "Remote Sensing Application", 
+                                                    ifelse(bennett_nodes$category == "RSH", "Remote Sensing History", "Science and Technology Studies"))))
+                                      
+p <- forceNetwork(Links = bennett, Nodes = bennett_nodes, Source = "Source", Target = "Target", Value = "value", NodeID = "name", Nodesize = "citecount", Group = "category_full", zoom = TRUE, linkDistance = 200, arrows = FALSE, legend = TRUE, fontFamily = "Avenir", fontSize = 14)
 
-p <- forceNetwork(Links = bennett, Nodes = bennett_nodes, Source = "Source", Target = "Target", Value = "value", NodeID = "name", Nodesize = "citecount", Group = "category", zoom = TRUE, linkDistance = 200, arrows = FALSE, legend = TRUE)
+p$x$nodes$hyperlink <- lapply(bennett_nodes$title, function(x) {
+  paste0("https://scholar.google.com/scholar?hl=en&as_sdt=0%2C30&q=", URLencode(x))
+  })
 
-p$x$nodes$hyperlink <- paste0("https://scholar.google.com/scholar?hl=en&as_sdt=0%2C30&q=", URLencode(bennett$title))
 
-?forceNetwork
+
+p$x$options$clickAction <- 'window.open(d.hyperlink)'
+
+p
+
+saveNetwork(p, file = "bennett_bib.html")
+
